6b61aa8ad59e2dec05f3d37e04fe642c9ad66733
mimi <hold1593@naver.com>
2022-12-05 18:13:44 +0900

    modify test

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,6 +5,7 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요~");
 		System.out.println("Git 연습용 파일입니다!");
+		System.out.println("Git 수정해보겠습니다");
 	}
 
 }
